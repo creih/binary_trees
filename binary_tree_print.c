5ce39ea7862ee2c58c96f06fa6fd54dd23fdfa13
creih <iherr.9@gmail.com>
2024-03-26 21:35:34 +0200

    can't find where the issue is

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -3,11 +3,8 @@
 #include <string.h>
 #include "binary_trees.h"
 
-/* Original code from http://stackoverflow.com/a/13755911/5184480 */
-
 /**
  * print_t - Stores recursively each level in an array of strings
- *
  * @tree: Pointer to the node to print
  * @offset: Offset to print
  * @depth: Depth of the node
