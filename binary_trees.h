da973f7b0d0ef651d8d5b8e3f587d74bf675784f
creih <iherr.9@gmail.com>
2024-03-26 21:13:51 +0200

    added the print function and its dependencies

--- binary_trees.h
+++ binary_trees.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <stdef.h>
 /**
  * struct binary_tree_s - Binary tree node
  * @n: Integer stored in the node
@@ -18,8 +18,8 @@ struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
 
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 #endif
