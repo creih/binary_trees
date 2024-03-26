8886a1d9b477e33e18551b7daa6354266467c9b7
creih <iherr.9@gmail.com>
2024-03-26 12:31:58 -0700

    trying to debug some errors

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,7 +7,8 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node_nshya = (binary_tree_t*)malloc(sizeof(binary_tree_t));
+	binary_tree_t *node_nshya = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
 	if (node_nshya == NULL)
 		return (NULL);
 	node_nshya->n = value;
--- binary_trees.h
+++ binary_trees.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdef.h>
+#include <stddef.h>
 /**
  * struct binary_tree_s - Binary tree node
  * @n: Integer stored in the node
