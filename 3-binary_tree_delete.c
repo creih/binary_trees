8d8a001bbeccf7c6b87e0de4231459af04126303
creih <iherr.9@gmail.com>
2024-03-27 21:21:46 +0200

    made the delete function in task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return;
+    free(tree);
+    tree = NULL;
+}
