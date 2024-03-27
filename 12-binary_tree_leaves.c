c2a7924904c06ed0b73424eb9455c6ad8d0f8ba8
creih <iherr.9@gmail.com>
2024-03-27 15:02:30 -0700

    task 12 resoled the paranthesis issue on return

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,11 +7,12 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return 0;
-    if (tree->left == NULL && tree->right == NULL)
-        return 1;
-    size_t leaves_left = binary_tree_leaves(tree->left);
-    size_t leaves_right = binary_tree_leaves(tree->right);
-    return leaves_left + leaves_right;
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	size_t leaves_left = binary_tree_leaves(tree->left);
+	size_t leaves_right = binary_tree_leaves(tree->right);
+
+	return (leaves_left + leaves_right);
 }
