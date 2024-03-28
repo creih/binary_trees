b7b4ee9ddd2ace95d70c4439920d83e381b3f8e8
creih <iherr.9@gmail.com>
2024-03-28 00:46:46 -0700

    task 13 remade was very sloppy

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,14 +7,15 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    int r;
+	int r;
 
-    if (tree == NULL)
-        return 0;
-
-    if (tree->left != NULL || tree->right != NULL)
-        r = 1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
-        return r;
-    else
-        return 0;
+	if (tree == NULL)
+		return 0;
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		r = 1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+		return r;
+	}
+	else
+		return 0;
 }
