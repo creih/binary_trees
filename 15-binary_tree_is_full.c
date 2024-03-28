84e6280a80deb3aa06e741ab7addb7e2619b98c3
creih <iherr.9@gmail.com>
2024-03-28 03:42:10 -0700

    task 15 done and ready for check

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,11 +7,14 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int r;
+
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
-	return binary_tree_is_full(tree->left) && binary_tree_is_full(tree->righ);
+	r = binary_tree_is_full(tree->left) && binary_tree_is_full(tree->righ);
+	return (r);
 }
