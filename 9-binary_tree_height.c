d79906c488006492b4a88d0257daa208d6461132
creih <iherr.9@gmail.com>
2024-03-29 12:28:05 -0700

    task 9reverted

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,7 +7,7 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_left, height_right, r;
+	size_t height_left, height_right;
 
 	if (tree == NULL)
 		return (0);
@@ -15,6 +15,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	height_left = binary_tree_height(tree->left);
 	height_right = binary_tree_height(tree->right);
 
-	r = (height_left > height_right ? height_left + 1 : height_right + 1);
-	return (r);
+	return (height_left > height_right ? height_left : height_right) + 1;
 }
